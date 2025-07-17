da4cf23b97878e868ee0f28013217c6e854f1f2d
Ayush Sharma <ayush49.sharma@gmail.com>
2025-07-17 20:08:58 +0530

    Update settings.py

--- mindmate/settings.py
+++ mindmate/settings.py
@@ -151,24 +151,3 @@ MESSAGE_TAGS = {
 CRISPY_ALLOWED_TEMPLATE_PACKS = ["bootstrap5"]
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
-import os
-from pathlib import Path
-from django.contrib.messages import constants as messages
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-SECRET_KEY = os.environ.get('SECRET_KEY')
-if not SECRET_KEY:
-    raise ValueError("SECRET_KEY is not set in environment variables")
-
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
-
-
-CSRF_TRUSTED_ORIGINS = [
-    'https://mindmate.onrender.com', 
-]
-
-
