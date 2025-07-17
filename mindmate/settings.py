9cda2e330c4a05992921f69a0adff4008832ec1c
Ayush Sharma <ayush49.sharma@gmail.com>
2025-07-17 18:22:59 +0530

    Update settings.py

--- mindmate/settings.py
+++ mindmate/settings.py
@@ -155,3 +155,8 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-dev-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://your-app-name.onrender.com',
+]
+
