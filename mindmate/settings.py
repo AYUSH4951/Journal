341673e71fddf01483dd894569c492af4dac5146
Ayush Sharma <ayush49.sharma@gmail.com>
2025-07-17 18:07:25 +0530

    Update settings.py

--- mindmate/settings.py
+++ mindmate/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3y+(s7qcpw8vwxxp20cy(+f-nnq*txg8p79hmz#tl)oqrc&%o%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
